d3d271fb326022c636d3281e2243681ab518dc82
yoonjaejasonlee <tug95015@temple.edu>
2022-09-05 08:54:04 -0400

    2022/09/03 Daily Challenge

---
+++ python/Q967.py
@@ -0,0 +1,16 @@
+# 967. Numbers With Same Consecutive Differences
+# https://leetcode.com/problems/numbers-with-same-consecutive-differences/
+
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        ans = []
+        d = deque((1, d) for d in range(1, 10))
+        while d:
+            pos, num = d.pop()
+            if pos == n:
+                ans.append(num)
+            else:
+                for j in range(10):
+                    if abs(num % 10 - j) == k:
+                        d.append((pos + 1, num * 10 + j))
+        return ans)
