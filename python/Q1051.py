83afff611af1906236396146e2f93a68831335dd
Jason Lee <jnslee311@gmail.com>
2022-09-18 23:59:15 -0400

    1051. Height Checker

---
+++ python/Q1051.py
@@ -0,0 +1,16 @@
+#1051. Height Checker
+#https://leetcode.com/problems/height-checker/
+
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        temp = sorted(heights)
+        counter = 0
+        
+        
+        for i in range(len(heights)):
+            if heights[i] != temp[i]:
+                counter += 1
+                
+        
+        return counter 
